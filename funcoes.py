56a77c0fdc339470aaed99987ca7986c5d29dd02
GustavoAntony <gustavoa9@al.insper.edu.br>
2023-03-02 17:39:47 -0300

    para string done

--- funcoes.py
+++ funcoes.py
@@ -3,16 +3,6 @@ ALFABETO = 'abcdefghijklmnopqrstuvwxyz '
 TAMANHO_ALFABETO = len(ALFABETO)
 MATRIZ_ALFABETO = np.identity(TAMANHO_ALFABETO,dtype=int)
 
-#FUNÇÕES ÚTEIS
-# def one_hot_individual(letra):
-#     # alfabeto = {'a' : 0, 'b': 1, 'c':2, 'd':3, 'e':4, 'f':5, 'g':6, 'h':7, 'i':8, 'j':9, 'k':10, 'l':11, 'm':12, 'n':13, 'o':14,'p':15,'q':16,'r':17,'s':18,'t':19,'u':20,'v':21,'w':22,'x':23,'y':24,'z':25}
-
-#     if letra not in alfabeto:
-#         return np.zeros(27)
-#     else:
-#         lista = []
-#         for a in range(len(alfabeto)):
-#             if letra == alfabeto[a]:
 
 def para_one_hot(string):
     contador = 0
@@ -27,6 +17,20 @@ def para_one_hot(string):
     matriz_palavra = matriz_palavra.T
     return matriz_palavra
 
-print(para_one_hot("gustavo"))
+
+
+
+def para_string(matriz):
+    palavra = ""
+    for coluna in matriz.T:
+        i = np.where(coluna == 1)[0][0]
+        palavra += ALFABETO[i]
+    return palavra
+
+mama = para_one_hot("gustavo")
+print(para_string(mama))
+    
+    
+
 
 
