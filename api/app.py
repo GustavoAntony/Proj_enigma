48f445835933f8e746b2578aacc5b2f12c50263c
C0D8 <matheusaguiar362@gmail.com>
2023-03-10 10:36:34 -0300

    Setup do pip

--- api/app.py
+++ api/app.py
@@ -8,33 +8,21 @@ app = Flask(__name__)
 def hello():
     return "Essa é uma api para cifrar e decifrar com a máquina enigma"
 
-@app.route("/", methods=['POST'])
+@app.route("/enigma", methods=['POST'])
 def cifra():                               #(mensagem, p, e) só recebe seed e a mensagem
 
     data = request.json
     seed = data.get('seed')
+    acao = data.get('acao')
     mensagem = data.get('mensagem')
-    # mensagem_one_hot = para_one_hot(mensagem)    
     permutada = np.roll(MATRIZ_ALFABETO, seed, 1)
     e = np.roll(permutada, seed, 1)
 
-    return enigma(mensagem, permutada, e)
+    if acao == 'cifrar':
+        return enigma(mensagem, permutada, e)
+    elif acao == 'decifrar':
+        return de_enigma(mensagem,permutada,e)
 
-
-
-@app.route("/de", methods=['POST'])
-def de_cifra():                               #(mensagem, p, e) só recebe seed e a mensagem
-
-    data = request.json
-    seed = data.get('seed')
-    mensagem = data.get('mensagem')
-    # mensagem_one_hot = para_one_hot(mensagem)
-    permutada = np.roll(MATRIZ_ALFABETO, seed, 1)
-    e = np.roll(permutada, seed, 1)
-
-
-    return de_enigma(mensagem,permutada,e)
   
-
 
 app.run()
---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup 
+
+
+setup(name="enigmatico",
+      version='0.1',
+      description="Apresenta funções de criptografia de decriptografia assi como a máquina enigma fazia",
+      packages=['enigma'],
+      author='Matheus e Gustavo',
+      zip_safe=False)
